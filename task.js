2acd16e0d01dad9dd6e6785f0b383ab52d5dcb10
Khaldoun <khaldounnraw@gmail.com>
2021-10-01 00:21:23 +0300

    solution for JavaScript task

--- task.js
+++ task.js
@@ -1,44 +1,40 @@
-let numbers = [
-    5,
-    8,
-    0,
-    1,
-    9,
-    11,
-    15,
-    16
-];
+let numbers = [5, 8, 0, 1, 9, 11, 15, 16];
+let immutableArray = Array.from(numbers); //making the array immutable
 
-console.log("Original numbers list: ", numbers)
+console.log("Original numbers list: ", numbers);
 
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] > numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
-    }
-}
+const BubbleSortAsc = (arr) => {
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr.length - i - 1; j++) {
+			//first correct: subtract the array from first itration "i"
+			if (arr[j] > arr[j + 1]) {
+				let left = arr[j]; //adding convenient naming to make it more readable
+				let right = arr[j + 1];
+				arr[j] = right; //second correct: replace the index to be the second iteration "j"
+				arr[j + 1] = left;
+			}
+		}
+	}
+	return arr;
+};
 
-console.log("Numbers list After sorting: ", numbers)
+console.log("Numbers list After sorting: ", BubbleSortAsc(immutableArray));
 
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] < numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
-    }
-}
+const BubbleSortDesc = (arr) => {
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr.length - 1; j++) {
+			if (arr[j] < arr[j + 1]) {
+				let left = arr[j];
+				let right = arr[j + 1];
+				arr[j] = right; //third correct: replace the index to be the second iteration "j"
+				arr[j + 1] = left;
+			}
+		}
+	}
+	return arr;
+};
 
-console.log("Numbers list After Desc sorting: ", numbers)
-
-
+console.log(
+	"Numbers list After Desc sorting: ",
+	BubbleSortDesc(immutableArray)
+); //here we invoked the function
